fe843a2fca79a1daf3638445b7c4eef6c65d496d
aliaksandrtseliuk <52308758+aliaksandrtseliuk@users.noreply.github.com>
2020-03-01 13:28:43 +0300

    feat: add solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,29 @@
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.push(value)
+    return this
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (position > 0 && position <= this.chain.length) {
+      this.chain.splice(position - 1, 1)
+      return this
+    }
+    this.chain = []
+    throw new Error()
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse()
+    return this
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let res = this.chain.map(item => '( ' + item + ' )')
+                        .join('~~')
+    this.chain = []
+    return res
   }
 };
 
